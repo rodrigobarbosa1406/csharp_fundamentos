fe054ae2562dfaac3dfac0dd0f0d206f8fd36291
rodrigobarbosa1406 <rodrigobarbosanb@hotmail.com>
2021-08-28 17:56:23 -0300

    Exercicios propostos - PARTE 2 (Aula 29)

--- Secao03/Secao03.cs
+++ Secao03/Secao03.cs
@@ -26,17 +26,42 @@ namespace Secao03 {
                         Console.WriteLine();
 
                         if (exercicio == 1)
-                            exercicio01();
+                            aula24Exercicio01();
                         else if (exercicio == 2)
-                            exercicio02();
+                            aula24Exercicio02();
                         else if (exercicio == 3)
-                            exercicio03();
+                            aula24Exercicio03();
                         else if (exercicio == 4)
-                            exercicio04();
+                            aula24Exercicio04();
                         else if (exercicio == 5)
-                            exercicio05();
+                            aula24Exercicio05();
                         else if (exercicio == 6)
-                            exercicio06();
+                            aula24Exercicio06();
+
+                        Console.WriteLine();
+                    }
+                    else if (aula == 29)
+                    {
+                        Console.Write("Qual exercício você quer executar? ");
+                        int exercicio = int.Parse(Console.ReadLine());
+                        Console.WriteLine();
+
+                        if (exercicio == 1)
+                            aula29Exercicio01();
+                        else if (exercicio == 2)
+                            aula29Exercicio02();
+                        else if (exercicio == 3)
+                            aula29Exercicio03();
+                        else if (exercicio == 4)
+                            aula29Exercicio04();
+                        else if (exercicio == 5)
+                            aula29Exercicio05();
+                        else if (exercicio == 6)
+                            aula29Exercicio06();
+                        else if (exercicio == 7)
+                            aula29Exercicio07();
+                        else if (exercicio == 8)
+                            aula29Exercicio08();
 
                         Console.WriteLine();
                     }
@@ -81,7 +106,7 @@ namespace Secao03 {
             Console.WriteLine($"Separador decimal invariante culture: {medida.ToString("F3", CultureInfo.InvariantCulture)}");
         }
 
-        private static void exercicio01()
+        private static void aula24Exercicio01()
         {
             Console.Write("Informe o primeiro número: ");
             int n1 = int.Parse(Console.ReadLine());
@@ -94,7 +119,7 @@ namespace Secao03 {
             Console.WriteLine("SOMA = " + soma);
         }
 
-        private static void exercicio02()
+        private static void aula24Exercicio02()
         {
             double pi = 3.14159;
 
@@ -105,7 +130,7 @@ namespace Secao03 {
 
             Console.WriteLine($"A = {area.ToString("F4", CultureInfo.InvariantCulture)}");
         }
-        private static void exercicio03()
+        private static void aula24Exercicio03()
         {
             Console.WriteLine("Informe os valores de A, B, C e D...");
             Console.Write("A: ");
@@ -125,7 +150,7 @@ namespace Secao03 {
             Console.WriteLine("DIFERENCA = " + diferenca);
         }
 
-        private static void exercicio04()
+        private static void aula24Exercicio04()
         {
             Console.Write("Informe o número do funcionário: ");
             int numero = int.Parse(Console.ReadLine());
@@ -143,7 +168,7 @@ namespace Secao03 {
             Console.WriteLine($"SALÁRIO = R$ {salario.ToString("F2", CultureInfo.InvariantCulture)}");
         }
 
-        private static void exercicio05()
+        private static void aula24Exercicio05()
         {
             Console.Write("Informe o código, a quantidade e o valor unitário da peça 1: ");
             string[] peca1 = Console.ReadLine().Split(' ');
@@ -163,7 +188,7 @@ namespace Secao03 {
             Console.WriteLine($"VALOR A PAGAR: R$ {valorPagar.ToString("F2", CultureInfo.InvariantCulture)}");
         }
 
-        private static void exercicio06()
+        private static void aula24Exercicio06()
         {
             Console.WriteLine("Informe os valores de A, B e C...");
             string[] valores = Console.ReadLine().Split(' ');
@@ -213,6 +238,160 @@ namespace Secao03 {
             double retangulo = lado1 * lado2;
             
             return retangulo;
+        }
+
+        private static void aula29Exercicio01()
+        {
+            Console.Write("Informe um número inteiro: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero < 0)
+                Console.Write("NEGATIVO");
+            else if (numero >= 0)
+                Console.Write("NÃO NEGATIVO");
+        }
+
+        private static void aula29Exercicio02()
+        {
+            Console.Write("Informe um número inteiro: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero % 2 == 0)
+                Console.Write("PAR");
+            else
+                Console.Write("ÍMPAR");
+        }
+
+        private static void aula29Exercicio03()
+        {
+            Console.Write("Informe o primeiro número: ");
+            int primeiroNumero = int.Parse(Console.ReadLine());
+
+            Console.Write("Informe o segundo número: ");
+            int segundoNumero = int.Parse(Console.ReadLine());
+
+            int resto = 0;
+
+            if (primeiroNumero >= segundoNumero)
+                resto = primeiroNumero % segundoNumero;
+            else
+                resto = segundoNumero % primeiroNumero;
+            
+            if (resto == 0)
+                Console.Write("São múltiplos");
+            else
+                Console.Write("Não são múltiplos");
+        }
+
+        private static void aula29Exercicio04()
+        {
+            Console.Write("Informe a hora inicial: ");
+            int horaInicial = int.Parse(Console.ReadLine());
+
+            Console.Write("Informe a hora final: ");
+            int horaFinal = int.Parse(Console.ReadLine());
+
+            int duracao = 0;
+
+            if (horaInicial < horaFinal)
+                duracao = horaFinal - horaInicial;
+            else if (horaInicial > horaFinal)
+                duracao = (24 - horaInicial) + horaFinal;
+            else if (horaInicial == horaFinal)
+                duracao = 24;
+
+            Console.Write("O JOGO DUROU " + duracao + " HORA(S)");
+        }
+
+        private static void aula29Exercicio05()
+        {
+            Console.Write("Informe o código do produto: ");
+            int codigo = int.Parse(Console.ReadLine());
+
+            Console.Write("Informe a quantidade: ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            double valorProduto = 0.00;
+            double valorTotal = 0.00;
+
+            if (codigo == 1)
+                valorProduto = 4.00;
+            else if (codigo == 2)
+                valorProduto = 4.50;
+            else if (codigo == 3)
+                valorProduto = 5.00;
+            else if (codigo == 4)
+                valorProduto = 2.00;
+            else if (codigo == 5)
+                valorProduto = 1.50;
+
+            valorTotal = valorProduto * quantidade;
+
+            Console.Write($"Total: R$ {valorTotal.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+
+        private static void aula29Exercicio06()
+        {
+            Console.Write("Informe um valor: ");
+            double valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            if (valor >= 0.00 && valor <= 25.00)
+                Console.Write("Intervalo [0,25]");
+            else if (valor >= 25.01 && valor <= 50.00)
+                Console.Write("Intervalo [25,50]");
+            else if (valor >= 50.01 && valor <= 75.00)
+                Console.Write("Intervalo [50,75]");
+            else if (valor >= 75.01 && valor <= 100.00)
+                Console.Write("Intervalo [75,100]");
+            else
+                Console.Write("Fora do intervalo");
+        }
+
+        private static void aula29Exercicio07()
+        {
+            Console.Write("Informe o valor de X: ");
+            double X = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Informe o valor de Y: ");
+            double Y = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            if (X == 0.0 && Y == 0.0)
+                Console.Write("Origem");
+            else if (X > 0.0 && Y > 0.0)
+                Console.Write("Q1");
+            else if (X < 0.0 && Y > 0.0)
+                Console.Write("Q2");
+            else if (X < 0.0 && Y < 0.0)
+                Console.Write("Q3");
+            else if (X > 0.0 && Y < 0.0)
+                Console.Write("Q4");
+        }
+
+        private static void aula29Exercicio08()
+        {
+            Console.Write("Informe o salário: ");
+            double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double valorIsento = 2000.00;
+            double valorImposto = 0.0;
+            double baseCalculo = 0.0;
+
+            if (salario >= 2000.01 && salario <= 3000.00){
+                baseCalculo = salario - valorIsento;
+                valorImposto = baseCalculo * 0.08;
+            } else if (salario >= 3000.01 && salario <= 4500.00){
+                baseCalculo = salario - valorIsento;
+                valorImposto = (999.99 * 0.08) + ((baseCalculo - 999.99) * 0.18);
+            } else if (salario > 4500.01)
+            {
+                baseCalculo = salario - valorIsento;
+                valorImposto = (999.99 * 0.08) + (1499.99 * 0.18) + ((baseCalculo - 2499.98) * 0.28);
+            }
+
+            if (valorImposto == 0.00)
+                Console.Write("Isento");
+            else
+                Console.Write($"R$ {valorImposto.ToString("F2", CultureInfo.InvariantCulture)}");
         }
     }
 }
